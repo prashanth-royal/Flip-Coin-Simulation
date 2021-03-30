ed13d381e6ec4c13f5660893a5e2669e75702784
Prashanth <prashanth.royal2046@gmail.com>
2021-03-30 12:45:11 +0530

    Added: Difference of 2 points

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,11 +4,13 @@ num=21
 heads=0
 tails=0
 
+function reCoinFlip(){
+
 	while [ $heads -lt $num ] && [ $tails -lt $num ]
 	do
 		coinFlip=$((RANDOM%2))
 
-	if [ $coinFlip -eq 1 ]
+		if [ $coinFlip -eq 1 ]
 		then ((heads++))
 			echo HEADS
 							HEADS=$(($heads+1))
@@ -17,13 +19,28 @@ tails=0
 							TAILS=$(($tails+1))
 	fi
 done
+}
+
+	while [ $heads -lt $num ] && [ $tails -lt $num ]
+   do
+      coinFlip=$((RANDOM%2))
+
+      if [ $coinFlip -eq 1 ]
+      then ((heads++))
+         echo HEADS
+                     HEADS=$(($heads+1))
+      else ((tails++))
+         echo TAILS
+                     TAILS=$(($tails+1))
+   fi
+done
 
 	if [ $heads -eq $tails ]
-	then
+	then reCoinFlip
 			echo "TIE"
 	elif [ $heads -gt $tails ]
 	then
-			echo "HEADS won $heads times"
+			echo "HEADS won by $heads points"
 	else
-			echo "TAILS won $tails times"
+			echo "TAILS won by $tails points"
 fi
